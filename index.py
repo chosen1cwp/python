ddf0a1ab9150859c4191f6848e37e8f159e37fee
chosen1cwp <850029269@qq.com>
2018-08-27 15:15:29 +0800

    first

---
+++ index.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#print('caoniam','who am i', 'who are you!');
+#print(200+101);
+#name=input('请输入你的姓名进入系统');
+#if name == '蔡文鹏':
+#    print('输入正确',name,'\"先生\"');
+#else:
+#   print(r'你是\'谁\'，输入错误\n了');
+
+
+print('''line1
+line2
+line3''')
+
+age = input('请输入年龄：');
+print('你输入的年龄是：',age,'岁');
+print(type(age));
+#age = int(age);
+print(type(age));
+if age >= 18:
+    print('你已成年');
+else:
+    print('小毛孩，你还未成年');
+
+cwp = 123;
+print(cwp);
+cwp = '蔡文鹏';
+print(cwp);
+
+
